a74828279741ab815e9b7cebea69fbb1ef80a09a
Devanand Yadav <devanandraj2002@gmail.com>
2023-03-31 19:42:03 +0530

    more file added

---
+++ grade.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void * maxi
+}
+
+void * second_maximum(void * args) {
+
+}
+
+void * Find_Xor(void * args) {
+
+}
+
+int main()
+{
+    // struct Classroom obj;
+    // scanf("%d", &obj.n);
+    // int i;
+    // for(i=0; i<obj.n; i++){
+    //     scanf("%d", &obj.grades[i]);
+    // }
+
+}
